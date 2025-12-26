1786d919b22c7194fb02fdf943098eac25b713d0
facubrun <facundobrun0303@gmail.com>
2025-12-26 09:58:44 -0300

    add composer autoload files

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once  '../vendor/autoload.php';
+
+use Belur\HttpNotFoundException;
+use Belur\Router;
+
+$router = new Router();
+
+$router->get('/test', function() {
+    return 'GET OK.';
+});
+
+$router->post('/test', function() {
+    return 'POST OK.';
+});
+
+$router->put('/test', function() {
+    return 'PUT OK.';
+});
+
+$router->patch('/test', function() {
+    return 'PATCH OK.';
+});
+
+$router->delete('/test', function() {
+    return 'DELETE OK.';
+});
+
+
+try {
+    $action = $router->resolve();
+    print($action());
+} catch (HttpNotFoundException $e) {
+    print("Not Found");
+    http_response_code(404);
+}
