c3543bc5a53de16143e933d991a6b65d324755a6
Facundo Brun <facundobrun0303@gmail.com>
2025-12-27 15:31:58 -0300

    ejercicio 3 - response test

---
+++ tests/Http/ResponseTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Lune\Tests\Http;
+
+use Belur\Http\HttpMethod;
+use Belur\Http\Response;
+use Belur\Server\Server;
+use PHPUnit\Framework\TestCase;
+
+class ResponseTest extends TestCase {
+
+    public function test_json_response_is_constructed_correctly() {
+        $data = ['product' => 'test product', 'price' => 100];
+        $response = Response::json($data);
+
+        $this->assertEquals(200, $response->status());
+        $this->assertEquals(['content-type' => 'application/json'], $response->headers());
+        $this->assertEquals(json_encode($data), $response->body());
+    }
+
+    public function test_text_response_is_constructed_correctly() {
+        $response = Response::text('test text response');
+
+        $this->assertEquals(200, $response->status());
+        $this->assertEquals(['content-type' => 'text/plain'], $response->headers());
+        $this->assertEquals('test text response', $response->body());
+    }
+
+    public function test_redirect_response_is_constructed_correctly() {
+        $response = Response::redirect('/redirect/path');
+
+        $this->assertEquals(302, $response->status());
+        $this->assertEquals(['location' => '/redirect/path'], $response->headers());
+        $this->assertEquals('', $response->body());
+    }
+
+    public function test_prepare_method_removes_content_headers_if_there_is_no_content() {
+        $response = new Response();
+        $response->setContentType('text/plain')
+                ->setBody(null)
+                ->prepare();    
+        
+        $this->assertArrayNotHasKey('content-type', $response->headers());
+        $this->assertArrayNotHasKey('content-length', $response->headers());
+    }
+
+    public function test_prepare_method_adds_content_length_header_if_there_is_content() {
+        $response = new Response();
+        $response->setBody('test')
+                ->prepare();
+        
+        $this->assertEquals('4', $response->headers()['content-length']);
+    }
+}
