c32b9f5ca75c02214cd6ffa6bba4aa6b57b91b39
facubrun <facundobrun0303@gmail.com>
2025-12-30 14:26:35 -0300

    half validation rules v2

--- src/Validation/Rule.php
+++ src/Validation/Rule.php
@@ -3,10 +3,20 @@
 namespace Belur\Validation;
 
 use Belur\Validation\Rules\Email;
+use Belur\Validation\Rules\Required;
+use Belur\Validation\Rules\RequiredWith;
 use Belur\Validation\Rules\ValidationRule;
 
 class Rule {
     public static function email(): ValidationRule {
         return new Email();
+    }
+
+    public static function required(): ValidationRule {
+        return new Required();
+    }
+
+    public static function requiredWith(string $withField): ValidationRule {
+        return new RequiredWith($withField);
     }
 }
--- src/Validation/Rules/RequiredWith.php
+++ src/Validation/Rules/RequiredWith.php
@@ -3,10 +3,20 @@
 namespace Belur\Validation\Rules;
 
 class RequiredWith implements ValidationRule {
-    public function message(): string {
-        return 'The field is required when the other specified fields are present.';
+    protected string $withField;
+
+    public function __construct(string $withField) {
+        $this->withField = $withField;
     }
+
+    public function message(): string {
+        return "This field is required when {$this->withField} is present.";
+    }
+
     public function isValid(string $field, string $data): bool {
-        return !empty($data);
+        if(isset($data[$this->withField]) && ($data[$this->withField])) {
+            return isset($data[$field]) && !empty($data[$field]);
+        }
+        return true;
     }
 }
