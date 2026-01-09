4475c934f136fd52473bf8157987a5766fda36cc
Facundo Brun <facundobrun0303@gmail.com>
2026-01-09 17:43:21 -0300

    auth login

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,13 @@ use Belur\Routing\Route;
 
 use function Belur\Helpers\app;
 
-Route::get('/', fn ($request) => Response::text('Belur Framework'));
+Route::get('/', function ($request) {
+    if (isGuest()) {
+        return Response::text('Guest');
+    }
+
+    return Response::text(auth()->name);
+});
 
 Route::get('/form', fn ($request) => view('form', []));
 
@@ -30,5 +36,28 @@ Route::post('/register', function ($request) {
 
     $user = User::firstWhere('email', $data['email']);
     $user->login();
+    return redirect('/');
+});
+
+Route::get('/login', fn ($request) => view('auth/login', []));
+
+Route::post('/login', function ($request) {
+    $data = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
+
+    $user = User::firstWhere('email', $data['email']);
+    
+    if (is_null($user) || !app(Hasher::class)->verify($data['password'], $user->password)) {
+        return back()->withErrors(['email' => ['email' => 'Email not found']]);
+    }
+
+    $user->login();
+    return redirect('/');
+});
+
+Route::get('/logout', function ($request) {
+    auth()->logout();
     return redirect('/');
 });
